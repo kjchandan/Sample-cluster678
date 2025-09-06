51298e7427ea3fa464e87481eb03f4147ffa5257
CHANDAN K J <105358201+kjchandan@users.noreply.github.com>
2025-09-06 16:05:12 +0530

    Update main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -241,6 +241,14 @@ provider "helm" {
   }
 }
 
+# Kubernetes provider (needed for kubernetes_* and data resources)
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
