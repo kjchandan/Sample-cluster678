f10994b3cbca704ef1686a99b5c927c3d5bf7356
rakshitha-mayya <113757274+rakshitha-mayya@users.noreply.github.com>
2025-09-06 16:44:57 +0530

    Update main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -233,7 +233,7 @@ provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.akskube_admin_config[0]cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
 }
 
 # ------------------------------------------
